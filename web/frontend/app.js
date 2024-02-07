dce18c2d542169e4dfcd9b8130ebaf03d1d6318c
Yassin Sassi <we.yassin@gmail.com>
2024-02-07 10:09:42 +0100

    deleted frontend and added it to own repo

--- web/frontend/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const routes = require('./routes/index');
-
-const app = express();
-app.use(express.static('public'));
-//app.use('/', routes);
-
-module.exports = app;
